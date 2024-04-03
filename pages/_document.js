241d0676883945de8403afd71ed8d25b8930a313
Anisat Ahmed <anisatakinbani13@gmail.com>
2024-04-03 14:33:40 -0500

    update this repo

--- pages/_document.js
+++ pages/_document.js
@@ -28,7 +28,7 @@ export default class MyDocument extends Document {
           />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
             rel="stylesheet"
--- pages/about.js
+++ pages/about.js
@@ -72,14 +72,14 @@ const About = () => {
         </AboutHeading>
         <AboutContent>
           <p>
-            Hey there, I'm Anisat Ahmed, a front-end engineer and technical writer currently immersed in the world of Creative Technologies as I pursue my Master's degree in the United States.
+            Hey there, I&apos;m Anisat Ahmed, a front-end engineer and technical writer currently immersed in the world of Creative Technologies as I pursue my Master&apos;s degree in the United States.
           </p>
 
           <p>
             My playground? Crafting clean, intuitive user interfaces for websites and web apps. I thrive on simplicity, turning intricate ideas into seamless experiences, with ReactJS as my trusted companion.
           </p>
           <p>
-            Join me in the pursuit of digital elegance. Let's create interfaces that not only function flawlessly but also delight users at every click.
+            Join me in the pursuit of digital elegance. Let&apos;s create interfaces that not only function flawlessly but also delight users at every click.
           </p>
         </AboutContent>
       </AboutContainer>
